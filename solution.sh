de2b4365ffd9503d7891842b2a398c17f779f114
martiningaramo <tinchogaramo@gmail.com>
2022-09-27 14:12:36 +0200

    solution.sh fixed; Node Dockerfile image change to Alpine

--- solution.sh
+++ solution.sh
@@ -1,6 +1,6 @@
-docker build --tag nginximage hello-world-golang/
-docker build --tag nginximage hello-world-nodejs/
+docker build --tag goimage hello-world-golang/
+docker build --tag nodeimage_alpine hello-world-nodejs/
 docker build --tag nginximage hello-world-nginx/
 docker run -d --name goapp --network finalNetwork -p 3002:3002 goimage
-docker run -d --name nodeapp --network finalNetwork -p 3000:3000  nodeimage
+docker run -d --name nodeapp --network finalNetwork -p 3000:3000  nodeimage_alpine
 docker run -d --name nginxapp --network finalNetwork -p 80:18181 nginximage
